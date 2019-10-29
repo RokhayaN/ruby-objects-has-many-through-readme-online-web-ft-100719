4844dbbd9e9273ca4423e97a64b83abe2b340092
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-29 02:52:39 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,14 @@
 class Customer
+  attr_accessor :name :last :machine
+  @@all=[]
+  def name(name)
+    @name = name
+    @last = last
+  end
+  def self.all(all)
+    @@all
+  end
+  def relationship(customer)
+  
   
 end
